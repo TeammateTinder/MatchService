9ba1d191eeb4a9452293e097eba6926465d0cc44
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-05-28 12:31:47 +0200

    Rename Testing Folder

--- IntegrationTesting/obj/Debug/net8.0/IntegrationTesting.AssemblyInfo.cs
+++ IntegrationTesting/obj/Debug/net8.0/IntegrationTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntegrationTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4732b18577fecaf808b1d541e16bd01adcc2e735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1ace8db40111bf3e20200095458c1a16a51c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntegrationTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntegrationTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
