374e152d35833eb6bbbf1ca24f559f41bc8c36c7
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-05-28 13:04:07 +0200

    Update Tests

--- IntegrationTesting/MatchService.Tests.cs
+++ IntegrationTesting/MatchService.Tests.cs
@@ -23,52 +23,52 @@ namespace IntegrationTesting
             Assert.Equal(1, a+b);
         }
 
-        [Fact]
-        public void TestFormatMessageYes()
-        {
-            // Arrange
-            string? HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
-            if (string.IsNullOrEmpty(HostName))
-            {
-                HostName = "localhost";
-            }
-            ConnectionFactory factory = new ConnectionFactory() { HostName = HostName };
-            MatchService matchService = new MatchService(factory.CreateConnection());
-            int swiperID = 0;
-            int swipedID = 1;
-            YesOrNo yes = YesOrNo.y;
+        //[Fact]
+        //public void TestFormatMessageYes()
+        //{
+        //    // Arrange
+        //    string? HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
+        //    if (string.IsNullOrEmpty(HostName))
+        //    {
+        //        HostName = "localhost";
+        //    }
+        //    ConnectionFactory factory = new ConnectionFactory() { HostName = HostName };
+        //    MatchService matchService = new MatchService(factory.CreateConnection());
+        //    int swiperID = 0;
+        //    int swipedID = 1;
+        //    YesOrNo yes = YesOrNo.y;
 
-            // Act
-            string messageResult = matchService.FormatMessage(swiperID, swipedID, yes);
-            string expectedResult = $"{swiperID}:{swipedID}:{yes}";
+        //    // Act
+        //    string messageResult = matchService.FormatMessage(swiperID, swipedID, yes);
+        //    string expectedResult = $"{swiperID}:{swipedID}:{yes}";
 
-            // Assert
-            _logger.WriteLine($"Expected: {expectedResult}, Result: {messageResult}");
-            Assert.Equal(expectedResult, messageResult);
-        }
+        //    // Assert
+        //    _logger.WriteLine($"Expected: {expectedResult}, Result: {messageResult}");
+        //    Assert.Equal(expectedResult, messageResult);
+        //}
 
-        [Fact]
-        public void TestFormatMessageNo()
-        {
-            // Arrange
-            string? HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
-            if (string.IsNullOrEmpty(HostName))
-            {
-                HostName = "localhost";
-            }
-            ConnectionFactory factory = new ConnectionFactory() { HostName = HostName };
-            MatchService matchService = new MatchService(factory.CreateConnection());
-            int swiperID = 0;
-            int swipedID = 1;
-            YesOrNo no = YesOrNo.n;
+        //[Fact]
+        //public void TestFormatMessageNo()
+        //{
+        //    // Arrange
+        //    string? HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
+        //    if (string.IsNullOrEmpty(HostName))
+        //    {
+        //        HostName = "localhost";
+        //    }
+        //    ConnectionFactory factory = new ConnectionFactory() { HostName = HostName };
+        //    MatchService matchService = new MatchService(factory.CreateConnection());
+        //    int swiperID = 0;
+        //    int swipedID = 1;
+        //    YesOrNo no = YesOrNo.n;
 
-            // Act
-            string messageResult = matchService.FormatMessage(swiperID, swipedID, no);
-            string expectedResult = $"{swiperID}:{swipedID}:{no}";
+        //    // Act
+        //    string messageResult = matchService.FormatMessage(swiperID, swipedID, no);
+        //    string expectedResult = $"{swiperID}:{swipedID}:{no}";
 
-            // Assert
-            _logger.WriteLine($"Expected: {expectedResult}, Result: {messageResult}");
-            Assert.Equal(expectedResult, messageResult);
-        }
+        //    // Assert
+        //    _logger.WriteLine($"Expected: {expectedResult}, Result: {messageResult}");
+        //    Assert.Equal(expectedResult, messageResult);
+        //}
     }
 }
