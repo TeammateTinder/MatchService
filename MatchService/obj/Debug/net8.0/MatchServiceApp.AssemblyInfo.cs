b57430649414ef12cd545498d42fb14d5d84f987
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-05-28 12:32:43 +0200

    Rename Testing Folder

--- MatchService/obj/Debug/net8.0/MatchServiceApp.AssemblyInfo.cs
+++ MatchService/obj/Debug/net8.0/MatchServiceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MatchServiceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4732b18577fecaf808b1d541e16bd01adcc2e735")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1ace8db40111bf3e20200095458c1a16a51c62")]
 [assembly: System.Reflection.AssemblyProductAttribute("MatchServiceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MatchServiceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
